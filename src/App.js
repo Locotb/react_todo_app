7b6774a4eee95748744b68e2587fd837ddeab633
Locotb <VitalyPran@mail.ru>
2021-08-07 09:32:12 +0300

    Basic version of todo list application implemented on React.js This version contains: - Request on jsonplaceholder to get tasks - Ability to add a task - Ability to remove a task - Ability to display the task as completed and vice versa - Artificial delay to simulate the delay of the response from the server; at this time, the Loader is displayed - If all tasks are removed, the text "No todos" is displayed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import TodoList from "./todo/TodoList";
+import Context from './context';
+import AddTodo from './todo/AddTodo';
+import Loader from './Loader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
+
+    useEffect( () => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=7')
+            .then(response => response.json())
+            .then( todos => {
+                setTimeout( () => {
+                    setTodos(todos);
+                    setLoading(false)
+                }, 2000)
+            });
+    }, []);
+
+    function toggleTodo(id) {
+        setTodos(
+            todos.map(todo => {
+                if (todo.id === id) todo.completed = !todo.completed;
+                return todo;
+            })
+        );
+    }
+
+    function removeTodo(id) {
+        setTodos( todos.filter(todo => todo.id !== id) );
+    }
+
+    function addTodo(title) {
+        setTodos(todos.concat([
+            {
+                title,
+                id: Date.now(),
+                completed: false
+            }
+        ]));
+    }
+
+    return (
+        <Context.Provider value={{ removeTodo }}>
+            <div className="wrapper">
+                <h1>Todo application</h1>
+                <h2>Todo list</h2>
+                <AddTodo onCreate={addTodo} />
+                {todos.length ? (
+                    <TodoList todos={todos} onToggle={toggleTodo} />
+                ) : loading ? <Loader /> : (
+                    <p>No todos</p>
+                )}
+            </div>
+        </Context.Provider>
+    );
 }
 
 export default App;
---
+++ src/Loader.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Loader = () => <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}><div class="lds-facebook"><div></div><div></div><div></div></div></div>
+
+export default Loader;
